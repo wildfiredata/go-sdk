75e1e5de484ce3d8852d6665a21972ee0ad90cff
wildfiredata <wildfiredataanalytics@gmail.com>
2024-02-20 19:27:37 +0800

    init

---
+++ examples/main.go
@@ -0,0 +1,180 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+
+	"github.com/WildfireData/go-sdk/src/wildfiredata"
+	"github.com/google/uuid"
+)
+
+type A struct {
+	Name  string
+	Time  time.Time
+	Event []B
+}
+
+type B struct {
+	Trigger string
+	Time    time.Time
+}
+
+func main() {
+	// enable console log
+	wildfiredata.SetLogLevel(wildfiredata.TDLogLevelError)
+
+	// e.g. init consumer, you can choose different consumer
+
+	//consumer, err := generateDebugConsumer() // TDDebugConsumer
+	//consumer, err := generateBatchConsumer() // TDBatchConsumer
+	consumer, err := generateLogConsumer() // TDLogConsumer
+
+	if err != nil {
+		// consumer init error
+	}
+	te := wildfiredata.New(consumer)
+
+	defer func() {
+		err = te.Close()
+		if err != nil {
+			fmt.Printf("TE close error: %v\n", err.Error())
+		} else {
+			fmt.Println("[Example] TE close success")
+		}
+	}()
+
+	te.SetSuperProperties(map[string]interface{}{
+		"super_is_date":   time.Now(),
+		"super_is_bool":   true,
+		"super_is_string": "hello",
+		"super_is_num":    15.6,
+	})
+
+	te.SetDynamicSuperProperties(func() map[string]interface{} {
+		result := make(map[string]interface{})
+		result["dynamic_super_name"] = "Tom"
+		result["dynamic_super_time"] = time.Now()
+		return result
+	})
+
+	customData := A{
+		"WildfireData",
+		time.Now(),
+		[]B{
+			{Trigger: "Now We Support", Time: time.Now()},
+			{Trigger: "User Custom Struct Data", Time: time.Now()},
+		},
+	}
+	accountId := "AA"
+	distinctId := "ABCDEF123456"
+	properties := map[string]interface{}{
+		// "#time" is preset properties in SDK. SDK would use default time if not set.
+		"#time": time.Now(),
+		// "#ip" is preset properties in SDK. SDK would use default ip address if not set.
+		"#ip":       "123.123.123.123",
+		"channel":   "ta",
+		"age":       1,
+		"isSuccess": true,
+		"birthday":  time.Now(),
+		"object": map[string]interface{}{
+			"key": "value",
+		},
+		"objectArr": []interface{}{
+			map[string]interface{}{
+				"key": "value",
+			},
+		},
+
+		"arr":     []string{"test1", "test2", "test3"},
+		"my_data": customData,
+		"time_1":  time.Now(),
+		"time_2":  "2022-12-12T22:22:22.333444555Z",
+		"time_3":  "2022-12-12T22:22:22.333+08:00",
+		"time_4":  "2022-12-12T22:22:22.333Z",
+	}
+
+	// sync example
+	syncExample(&te, accountId, distinctId, properties)
+
+	//// async example
+	//asyncExample(&te, accountId, distinctId, properties)
+	//
+	//// async with http server.
+	//mock_server.Start(func() {
+	//	asyncExample(&te, accountId, distinctId, properties)
+	//})
+}
+
+func syncExample(te *wildfiredata.TDAnalytics, accountId, distinctId string, properties map[string]interface{}) {
+	//sync
+	for j := 0; j < 1; j++ {
+		distinctId = randomString()
+		err := te.Track(accountId, distinctId, "view_page", properties)
+		if err != nil {
+			fmt.Println(err)
+		}
+	}
+	err := te.Flush()
+	if err == nil {
+		fmt.Println("[Example] TE flush success")
+	} else {
+		fmt.Printf("TE flush error: %v", err.Error())
+	}
+}
+
+func asyncExample(te *wildfiredata.TDAnalytics, accountId, distinctId string, properties map[string]interface{}) {
+	wg := sync.WaitGroup{}
+	for i := 0; i < 1; i++ {
+		wg.Add(1)
+		distinctId = randomString()
+
+		go func(index int, distinctId string) {
+			defer wg.Done()
+			for j := 0; j < 1; j++ {
+				err := te.Track(accountId, distinctId, fmt.Sprintf("ab___%v___%v", index, j), properties)
+				if err != nil {
+					fmt.Println(err)
+				}
+			}
+		}(i, distinctId)
+	}
+	wg.Wait()
+	err := te.Flush()
+	if err == nil {
+		fmt.Println("[Example] TE flush success")
+	} else {
+		fmt.Printf("TE flush error: %v", err.Error())
+	}
+}
+
+func randomString() string {
+	newUUID, _ := uuid.NewUUID()
+	return newUUID.String()
+}
+
+func generateLogConsumer() (wildfiredata.TDConsumer, error) {
+	// logConsumer config
+	config := wildfiredata.TDLogConsumerConfig{
+		FileNamePrefix: "test_prefix",
+		Directory:      "./log",
+		RotateMode:     wildfiredata.ROTATE_HOURLY,
+		FileSize:       200,
+	}
+	return wildfiredata.NewLogConsumerWithConfig(config)
+}
+
+func generateBatchConsumer() (wildfiredata.TDConsumer, error) {
+	config := wildfiredata.TDBatchConfig{
+		ServerUrl: "https://receiver-ta-uat.wildfiredata.cn",
+		AppId:     "your appId",
+		AutoFlush: true,
+		BatchSize: 100,
+		Interval:  20,
+	}
+	return wildfiredata.NewBatchConsumerWithConfig(config)
+}
+
+func generateDebugConsumer() (wildfiredata.TDConsumer, error) {
+	return wildfiredata.NewDebugConsumerWithDeviceId("https://receiver-ta-uat.wildfiredata.cn", "appid", false, "deviceId")
+}
